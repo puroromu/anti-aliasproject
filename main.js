c05151e966b304a1d2d17263a27a122669b6d42b
html2021 <78289310+html2021@users.noreply.github.com>
2023-05-23 18:25:12 +0900

    main

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
-const express = require('express')
-const app = express()
-const port = 80
+const express = require("express");
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  // リクエストパラメータのタイトルを取得します。
+  const title = req.query.title;
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  // JSON ファイルからタイトルに一致する記事を検索します。
+  const articles = JSON.parse(fs.readFileSync("main.json"));
+  const article = articles.find((article) => article.title === title);
+
+  // 記事が見つかった場合は、レスポンスに返します。
+  if (article) {
+    res.send(article);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+app.listen(80, () => {
+  console.log("Server is listening on port 3000");
+});
